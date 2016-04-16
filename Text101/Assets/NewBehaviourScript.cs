69810d2004fa442b4b1256fcaea0a0629afff471
unknown-user <unknown-user@fe80::e56c:8abd:eddb:7cc9%8>
2016-04-15 19:53:05 -0500

    2nd commit?

--- Text101/Assets/NewBehaviourScript.cs
+++ Text101/Assets/NewBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print ("nope");
+		print ("noper");
 	}
 	
 	// Update is called once per frame
